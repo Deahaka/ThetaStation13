4c2464f7601cef467e2f19c4ba95ee10f67161bd
Dushess0 <44201461+Dushess0@users.noreply.github.com>
2022-06-27 00:41:24 +0200

    change

--- Tools/gen_build_info.py
+++ Tools/gen_build_info.py
@@ -20,9 +20,9 @@ SERVER_FILES = [
 ]
 VERSION = os.environ['GITHUB_SHA']
 FORK_ID = "theta"
-BUILD_URL = f"https://theta.dushess.net/builds/{{FORK_VERSION}}/{FILE}"
-MANIFEST_URL = f"https://theta.dushess.net/manifests/{{FORK_VERSION}}/manifest.json"
-MANIFEST_DOWNLOAD_URL = f"https://theta.dushess.net/manifests/{{FORK_VERSION}}/manifest.json"
+BUILD_URL = f"https://theta.dushess.net/builds/{FORK_ID}/{{FORK_VERSION}}/{FILE}"
+MANIFEST_URL = f"https://theta.dushess.net/manifests/{FORK_ID}/manifest.json"
+MANIFEST_DOWNLOAD_URL = f"https://theta.dushess.net/manifests/{FORK_ID}/manifest.json"
 
 def main() -> None:
     client_file = os.path.join("release", FILE)
